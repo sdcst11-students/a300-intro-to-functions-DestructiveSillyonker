1ec5b731d734d6fbab0660782d39356a2955068b
DestructiveSillyonker <johnr636@deltalearns.ca>
2023-10-24 13:10:39 -0700

    Done

--- task1.py
+++ task1.py
@@ -1,6 +1,23 @@
 def A():
+    print("Hello")
 
 def B():
-
+    print("How are you")
 
 def C():
+    print("Hi")
+
+
+
+
+
+Input = input("enter a capital letter from A to C: ")
+
+if Input == "A":
+    A()
+
+if Input == "B":
+    B()
+
+if Input == "C":
+    C()
--- task2.py
+++ task2.py
@@ -8,3 +8,47 @@ displays instructions and how to play
 game()
 plays the games
 """
+import random
+
+
+
+
+
+
+
+
+def title():
+    print("input a number between 1 and 500\nyou have 10 trys\nif the number is close then guess higher or lower\nif its correct then you win\nno two playthroughs will be the same...")
+def game():
+    Num = random.randint(1,500)
+
+
+    for i in range(1,11):
+        guess= int(input("Enter a number: "))
+
+
+        if guess > Num:
+            print("too high")
+
+        if guess < Num:
+            print("too low")
+
+        if guess == Num:
+            print("Correct! thanks for playing!")
+            break
+    else:
+        print(f"{Num} was the Correct number! thanks for playing!")
+
+
+
+
+
+Ask = input("Have you played before? Y or N?: ")
+for i in Ask:
+
+
+    if Ask == "Y":
+        game()
+        
+    if Ask == "N":
+        title()
